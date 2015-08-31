a36668cf2de897d85993e3cee87aaf2ac39288f7
Monkey <m.albertofi@gmail.com>
2015-08-30 19:24:31 -0500

    initial commit

---
+++ testProject/compartir/src/compartir/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for compartir project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "compartir.settings")
+
+application = get_wsgi_application()
